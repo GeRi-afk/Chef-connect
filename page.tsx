e5df2ba680c915e3e74ceead5f9c7f6aa40acf5d
GeRi-afk <gerizaimi2024@gmail.com>
2025-07-02 22:48:07 +0100

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,9 @@
+export default function ProfilePage() {
+  return (
+    <main>
+      <h1>Profile</h1>
+      <p>Welcome to your profile page!</p>
+      {/* Add more profile details here */}
+    </main>
+  );
+}

85427aeefa17894b92854d2fcfd5628d6d01cb21
GeRi-afk <gerizaimi2024@gmail.com>
2025-07-04 20:57:33 +0100

    Add files via upload

---
+++ app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <main>
+      <h1>Login</h1>
+      <p>Please enter your credentials to log in.</p>
+    </main>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Welcome to Chef Connect!</h1>
+      <p>Your homepage is set up and working.</p>
+    </main>
+  );
+}

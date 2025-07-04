6a27cec317d762fd6ea54031cbb9b374c25f7918
GeRi-afk <gerizaimi2024@gmail.com>
2025-07-04 20:55:09 +0100

    Delete page1.tsx

--- page1.tsx
+++
@@ -1,9 +0,0 @@
-export default function ProfilePage() {
-  return (
-    <main>
-      <h1>Profile</h1>
-      <p>Welcome to your profile page!</p>
-      {/* Add more profile details here */}
-    </main>
-  );
-}
